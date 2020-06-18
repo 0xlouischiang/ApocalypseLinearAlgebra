80142775bd229db9fc67fa9060ca256c875b12dd
jihy <0xlouischiang@gmail.com>
2020-06-18 11:22:36 +0800

    modified load_func

--- load_and_test.py
+++ load_and_test.py
@@ -1,11 +1,9 @@
 import xlrd
 from scipy.stats import shapiro
 
-xl = xlrd.open_workbook(r'./data/20200101.xls')
-table = xl.sheets()[0]
-col = table.col_values(1)
-print(col)
-print("===============")
 
-# Shapiro test
-print(shapiro(col))
+def config_data():
+    xl = xlrd.open_workbook(r'./data/20200101.xls')
+    table = xl.sheets()[0]
+    col = table.col_values(1)
+    return col
